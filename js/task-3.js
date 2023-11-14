8ec0b51261495cf75ccd4172660b47c87542ddaf
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-11-14 20:43:23 +0400

    js task

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
 function getElementWidth(content, padding, border){
 
-   const getElementWidth= 2 * (padding + border) + content;
-};
+   return content - (padding + border) * 2;
+
+
+}
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
